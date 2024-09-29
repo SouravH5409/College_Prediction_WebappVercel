81ba8982f4572e95d840a01d0304a009250b4d74
VDevanarayan <octavius1983bc@gmail.com>
2024-09-29 22:47:47 +0530

    added extra features

--- college_pred/settings.py
+++ college_pred/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -133,3 +134,5 @@ LOGIN_URL = '/login/'
 LOGOUT_REDIRECT_URL = '/login/'  
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR / 'predictor/static/predictor']
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
