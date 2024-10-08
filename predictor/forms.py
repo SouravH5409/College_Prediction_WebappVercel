a8c9fcec8ad3c7441a391265659d4ded4a7466ec
VDevanarayan <octavius1983bc@gmail.com>
2024-10-08 20:23:39 +0530

    updated result.html

--- predictor/forms.py
+++ predictor/forms.py
@@ -31,16 +31,33 @@ class UserInputForm(forms.Form):
         physics_marks = cleaned_data.get('physics_marks')
         chemistry_marks = cleaned_data.get('chemistry_marks')
         maths_marks = cleaned_data.get('maths_marks')
+        keam_score = cleaned_data.get('keam_score') 
+
+        if keam_score is not None and keam_score < 20:
+            self.add_error('keam_score', 'KEAM eligibility requires a minimum score of 20 (10 in each paper).')
 
         if board in ['CBSE', 'ICSE']:
             if (physics_marks > 100 or chemistry_marks > 100 or maths_marks > 100):
                 self.add_error('physics_marks', 'Marks for each subject should be out of 100 for CBSE/ICSE.')
                 self.add_error('chemistry_marks', 'Marks for each subject should be out of 100 for CBSE/ICSE.')
                 self.add_error('maths_marks', 'Marks for each subject should be out of 100 for CBSE/ICSE.')
+            else:
+                if ((physics_marks + chemistry_marks)/2) < 45:
+                    self.add_error('physics_marks', r'Average of Physics and Chemistry must be 45% or more.')
+                    self.add_error('chemistry_marks', r'Average of Physics and Chemistry must be 45% or more.')
+                if maths_marks < 45:
+                    self.add_error('maths_marks', 'Maths marks must be at least 45%.')
+
         elif board == 'STATE':
             if (physics_marks > 120 or chemistry_marks > 120 or maths_marks > 100):
                 self.add_error('physics_marks', 'Marks for Physics should be out of 120 for State Board.')
                 self.add_error('chemistry_marks', 'Marks for Chemistry should be out of 120 for State Board.')
                 self.add_error('maths_marks', 'Marks for Maths should be out of 100 for State Board.')
+            else:
+                if ((physics_marks + chemistry_marks)/240*100) < 45:
+                    self.add_error('physics_marks', r'Average of Physics and Chemistry must be 45% or more.')
+                    self.add_error('chemistry_marks', r'Average of Physics and Chemistry must be 45% or more.')
+                if maths_marks < 45:
+                    self.add_error('maths_marks', 'Maths marks must be at least 45%.')    
 
         return cleaned_data
